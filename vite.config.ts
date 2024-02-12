505702959bae7c9ec3485513aba10460e57d01cc
afute <wu2380547815@outlook.com>
2024-02-12 10:41:25 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,28 @@
-import { crx } from "@crxjs/vite-plugin";
-import vue from "@vitejs/plugin-vue";
-import { defineConfig } from "vite";
+import path from "path";
+
 import manifest from "./src/manifest.json";
+
+import vue from "@vitejs/plugin-vue";
+import { crx } from "@crxjs/vite-plugin";
+
+import { defineConfig } from "vite";
 
 
 export default defineConfig({
-    plugins: [vue(), crx({ manifest })],
+    plugins: [
+        vue(),
+        crx({ manifest })
+    ],
     css: {
         preprocessorOptions: {
             less: {
                 javascriptEnabled: true
             }
+        }
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src")
         }
     }
 });
